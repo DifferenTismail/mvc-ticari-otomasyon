b8d45fb349194ad05ac33292d672a598d05520a9
İsmail Baran <ismailbaran04@gmail.com>
2023-10-28 22:30:22 +0300

    personel classı olıuşturuldu

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Personel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Personel
+    {
+        [Key]
+        public int PersonelID { get; set; }
+        public string PersonelAd { get; set; }
+        public string PersonelSoyad { get; set; }
+        public string PersonelGorsel { get; set; }
+    }
+}

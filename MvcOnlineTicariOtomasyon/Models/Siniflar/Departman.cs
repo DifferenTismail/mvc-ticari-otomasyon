f043d3938947a193f6687c01427036c16ca3083d
İsmail Baran <ismailbaran04@gmail.com>
2023-10-28 22:39:09 +0300

    departman classı eklendi

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Departman.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Departman
+    {
+        public int DepartmanID { get; set; }
+        public string DepartmanAd { get; set; }
+    }
+}

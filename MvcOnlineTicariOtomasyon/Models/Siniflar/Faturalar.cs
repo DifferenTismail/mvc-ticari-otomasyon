a95830b1a0e3cbdd9834f9e87f8ffe8e2d0315ea
İsmail Baran <ismailbaran04@gmail.com>
2023-10-28 22:46:27 +0300

    Faturalar classı oluşturuldu

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Faturalar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Faturalar
+    {
+        [Key]
+        public int FaturaID { get; set; }
+        public string FaturaSeriNo{ get; set; }
+        public string FaturaSiraNo{ get; set; }
+        public DateTime Tarih{ get; set; }
+        public string VergiDairesi{ get; set; }
+        public DateTime Saat{ get; set; }
+        public string TeslimEden{ get; set; }
+        public string TeslimAlan{ get; set; }
+
+    }
+}

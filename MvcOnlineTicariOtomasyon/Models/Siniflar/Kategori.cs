94806b91e33febaa112bbb087302568aa4c8547e
İsmail Baran <ismailbaran04@gmail.com>
2023-10-28 21:52:27 +0300

    ilk kayıt. Sınıflar klasörü açıldı. Kategori ile urun classı oluşturuldu

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Kategori.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Kategori
+    {
+        [Key]
+        public int KategoriID { get; set; }
+        public string KategoriAd { get; set; }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Urun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Urun
+    {
+        [Key]
+        public int UrunID { get; set; }
+        public string UrunAd { get; set; }
+        public string Marka { get; set; }
+        public short Stok { get; set; }
+        public decimal AlisFiyati{ get; set;}
+        public decimal SatisFiyati{ get; set;}
+        public bool Durum { get; set; }
+        public string UrunGorsel{ get; set; }
+
+
+    }
+}

ce5bc9e5af2f754c83e7530a830d76beed04529e
İsmail Baran <ismailbaran04@gmail.com>
2023-10-28 22:30:08 +0300

    cariler classı olıuşturuldu

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int CariID { get; set; }
+        public string CardiAd { get; set; }
+        public string CariSoyad { get; set; }
+        public string CariSehir { get; set; }
+        public string CariMail { get; set; }
+    }
+}

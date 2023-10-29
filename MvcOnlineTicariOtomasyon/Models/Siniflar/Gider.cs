8fd7edfd6a83fbd62b617be873d4fce106058beb
İsmail Baran <ismailbaran04@gmail.com>
2023-10-29 21:22:12 +0300

    gider classı oluşturuldu

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Gider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Gider
+    {
+        [Key]
+        public int GiderID{ get; set; }
+        public string  Aciklama{ get; set; }
+        public DateTime Tarih {  get; set; }
+        public decimal Tutar { get; set; }
+    }
+}

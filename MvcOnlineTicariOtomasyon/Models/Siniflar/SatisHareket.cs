a93d99e535461b8d7f9a7ce652438dc6965c1014
İsmail Baran <ismailbaran04@gmail.com>
2023-10-30 00:35:34 +0300

    Satış Hareket classı oluşturuldu

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/SatisHareket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class SatisHareket
+    {
+        [Key]
+        public int SatisID { get; set; }
+        //ürün 
+        //cari 
+        //personel
+
+        public DateTime Tarih{ get; set; }
+        public int Adet { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal ToplamTutar{ get; set; }
+    }
+}

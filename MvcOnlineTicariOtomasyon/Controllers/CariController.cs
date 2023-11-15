ed9d94732a71fb9a3a3857de1ad03bb21b57a959
İsmail Baran <ismailbaran04@gmail.com>
2023-11-16 00:35:56 +0300

    cari listeleme yapıldı

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        // GET: Cari
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Carilers.ToList();
+            return View(degerler);
+        }
+    }
+}

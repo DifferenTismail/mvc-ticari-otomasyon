d038006690a7a9fb2df9d17460d5c2c792a6f6ac
İsmail Baran <ismailbaran04@gmail.com>
2023-11-15 18:52:24 +0300

    departman ve personel arasındaki veri tabanı bağlantısındaki hata düzeltildi

--- MvcOnlineTicariOtomasyon/Models/Siniflar/Personel.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Personel.cs
@@ -24,6 +24,7 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         [StringLength(250)]
         public string PersonelGorsel { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
-        public Departman Departman { get; set; }
+        public int Departmanid { get; set; }
+        public virtual Departman Departman { get; set; }
     }
 }

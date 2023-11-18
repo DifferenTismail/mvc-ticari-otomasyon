db47338be040bee1014c3d57c17ab1c0567f196d
İsmail Baran <ismailbaran04@gmail.com>
2023-11-19 00:01:04 +0300

    personel kontroller müsteri satış ve personel eklendi

---
+++ MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+namespace MvcOnlineTicariOtomasyonControllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        Context c = new Context(); 
+        public ActionResult Index()
+        {
+            var degerler = c.Personels.ToList();
+            return View();
+        }
+    }
+}

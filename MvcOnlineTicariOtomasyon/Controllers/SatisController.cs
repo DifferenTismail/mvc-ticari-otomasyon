ce2628064e49e029315d0f8b44d7bf36eccb4f95
İsmail Baran <ismailbaran04@gmail.com>
2023-11-27 15:02:54 +0300

    satış sayfasının controllera kodları yazıldı

---
+++ MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.SatisHarekets.ToList();
+            return View(degerler);
+        }
+        //form yüklendiğinde çalışıcak olan kısım
+        [HttpGet]
+        public ActionResult YeniSatis() {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList()
+                select new SelectListItem {
+                    Text = x.UrunAd,
+                    Value = x.UrunID.ToString()
+                }).ToList();
+
+            List<SelectListItem> deger2 = (from x in c.Carilers.ToList()
+                select new SelectListItem {
+                    Text = x.CardiAd + " " + x.CariSoyad,
+                    Value = x.CariID.ToString()
+                }).ToList();
+
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                select new SelectListItem {
+                    Text = x.PersonelAd + " " + x.PersonelSoyad,
+                    Value = x.PersonelID.ToString()
+                }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+            return View();
+        }
+        //Satış yapma sayfası kodları
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket s) {
+            s.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            c.SatisHarekets.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}

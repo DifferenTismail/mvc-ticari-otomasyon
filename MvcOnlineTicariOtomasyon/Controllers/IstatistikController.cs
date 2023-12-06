dd4f3f17eb1409a666026b29e20ba5cccfd5217c
İsmail Baran <ismailbaran04@gmail.com>
2023-12-06 14:37:40 +0300

    istatiktik controller oluşturuldu

---
+++ MvcOnlineTicariOtomasyon/Controllers/IstatistikController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        // GET: Istatistik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

5b9c093376e6dd71f08c626e50489dd0d8351bc6
İsmail Baran <ismailbaran04@gmail.com>
2023-12-06 18:08:50 +0300

    galeri controller sayfası tamamlandı

---
+++ MvcOnlineTicariOtomasyon/Controllers/GaleriController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GaleriController : Controller
+    {
+        // GET: Galeri
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Uruns.ToList();
+            return View(degerler);
+        }
+    }
+}

09ecc5723b89cd9e15a2866f138eb375d80bae9c
İsmail Baran <ismailbaran04@gmail.com>
2023-12-11 15:11:14 +0300

    urun detay sayfasının controllerı tamamlandı

---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        // GET: UrunDetay
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+           // var degerler = c.Uruns.Where(x => x.UrunID == 1).ToList();
+           cs.Deger1 = c.Uruns.Where(x => x.UrunID == 1).ToList();
+           cs.Deger2 = c.Detays.Where(y => y.DetayID == 1).ToList();
+            return View(cs);
+        }
+
+    }
+}

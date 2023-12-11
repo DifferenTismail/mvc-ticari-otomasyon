5e992c425f36bb6c897b0522020f324d958f0e25
İsmail Baran <ismailbaran04@gmail.com>
2023-12-11 15:14:25 +0300

    detay tablosu contexte eklendi

--- MvcOnlineTicariOtomasyon/Models/Siniflar/Context.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Context.cs
@@ -17,5 +17,6 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         public DbSet<Personel> Personels { get; set; }
         public DbSet<SatisHareket> SatisHarekets{ get; set; }
         public DbSet<Urun> Uruns{ get; set; }
+        public DbSet<Detay> Detays { get; set;}
     }
 }

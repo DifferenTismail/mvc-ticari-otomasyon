210faf62b0fc344867e3ad684c5ad045cce2a8bb
İsmail Baran <ismailbaran04@gmail.com>
2023-12-11 20:56:31 +0300

    ortak veri tutmak için oluşturuldu

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/SinifGrup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class SinifGrup
+    {
+        public string Sehir { get; set; }
+        public int Sayi { get; set; }
+    }
+}

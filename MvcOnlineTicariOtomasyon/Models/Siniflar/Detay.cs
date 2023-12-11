3ea119c90c92e7c4bfa8eaa20138e71b2c4dc712
İsmail Baran <ismailbaran04@gmail.com>
2023-12-11 15:09:55 +0300

    ürünlerin detay sayfası ayarlandı

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Detay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Detay
+    {
+        [Key]
+        public int DetayID { get; set; }
+
+        [Column (TypeName = "Varchar")]
+        [StringLength(30)]
+        public string UrunAd{ get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(2000)]
+        public string UrunBilgi{ get; set; }
+    }
+}

0bf271ca3fc5e28db4817aeca86ad2d3f4e6a856
İsmail Baran <ismailbaran04@gmail.com>
2023-12-11 15:09:07 +0300

    urun birden fazla veri tabanı verisini bir ekrana yazdırmamızız sağlayacak olan class

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Class1
+    {
+        public IEnumerable<Urun> Deger1 { get; set; }
+        public IEnumerable<Detay> Deger2 { get; set; }
+
+    }
+}

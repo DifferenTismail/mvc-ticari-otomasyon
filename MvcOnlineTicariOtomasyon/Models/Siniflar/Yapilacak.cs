d32b4a6c15a8e56cad7939ad44c15381c9983fbb
İsmail Baran <ismailbaran04@gmail.com>
2023-12-14 01:29:15 +0300

    todo listin veri tabanı oluşturuldu

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Yapilacak.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Yapilacak
+    {
+        [Key]
+        public int YapicalakID { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(100)]
+        public string Baslik{ get; set; }
+
+        [Column(TypeName = "bit")]
+        public bool Durum { get; set; }
+
+
+    }
+}

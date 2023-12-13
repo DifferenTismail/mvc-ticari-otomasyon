8485769f11c2037c0618a8d3927760498d4a8bf9
İsmail Baran <ismailbaran04@gmail.com>
2023-12-14 01:28:28 +0300

    todo list kodları yazıldı

---
+++ MvcOnlineTicariOtomasyon/Controllers/YapilacakController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class YapilacakController : Controller
+    {
+        // GET: Yapilacak
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var deger1 = c.Carilers.Count().ToString();
+            ViewBag.d1 = deger1;
+
+            var deger2 = c.Uruns.Count().ToString();
+            ViewBag.d2 = deger2;
+
+            var deger3 = c.Kategoris.Count().ToString();
+            ViewBag.d3 = deger3;
+
+            var deger4 = (from x in c.Carilers select x.CariSehir).Distinct().Count().ToString();
+            ViewBag.d4 = deger4;
+
+            var yapilacaklar = c.Yapilacaks.ToList();
+            return View(yapilacaklar);
+        }
+    }
+}

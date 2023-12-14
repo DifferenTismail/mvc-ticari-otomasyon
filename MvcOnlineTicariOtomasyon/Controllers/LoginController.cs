0c21eb015b58c6ace78f678395dd4df582df3910
İsmail Baran <ismailbaran04@gmail.com>
2023-12-15 01:20:15 +0300

    cari login formu post işlemi için gerekli olan kodlar yazıldı

---
+++ MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public PartialViewResult Partial1()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Partial1(Cariler p)
+        {
+            c.Carilers.Add(p);
+            c.SaveChanges();
+            return PartialView();
+        }
+        [HttpGet]
+        public ActionResult CariLogin1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CariLogin1(Cariler ca)
+        {
+            var bilgiler = c.Carilers.FirstOrDefault(x => x.CariMail == ca.CariMail && x.CariSifre == ca.CariSifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.CariMail, false);
+                Session["CarilerMail"] = bilgiler.CariMail.ToString();
+                return RedirectToAction("Index", "CariPanel");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+        }
+    }
+}

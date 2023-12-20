d64c47e09aa49b10980897583fc1f885f1e3418d
İsmail Baran <ismailbaran04@gmail.com>
2023-12-21 00:59:52 +0300

    kargo detay ve takip sinifları oluşturuldu

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/KargoDetay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class KargoDetay
+    {
+        [Key]
+        public int KargoDetayID { get; set; }
+
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(300)]
+        public string Aciklama{ get; set; }
+
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(10)]
+        public string TakipKodu{ get; set; }
+
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(20)]
+        public string Personel{ get; set; }
+
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(25)]
+        public string Alici{ get; set; }
+        public DateTime Tarih{ get; set; }
+    }
+}

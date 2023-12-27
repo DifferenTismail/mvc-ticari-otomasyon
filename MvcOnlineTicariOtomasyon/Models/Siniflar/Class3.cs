dd29f876e3b62cb29866fd8b649f4875e6d68ce1
İsmail Baran <ismailbaran04@gmail.com>
2023-12-28 02:09:08 +0300

    class3 adında kategori için sınıf açıldı

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Class3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Class3
+    {
+        public IEnumerable<SelectListItem> Kategoriler { get; set; }
+        public IEnumerable<SelectListItem> Urunler{ get; set; }
+    }
+}

0990616ee202523aca05540e99f0898ee7b3f9b1
İsmail Baran <ismailbaran04@gmail.com>
2023-12-28 02:06:10 +0300

    authorize eklendi

--- MvcOnlineTicariOtomasyon/Global.asax.cs
+++ MvcOnlineTicariOtomasyon/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.EnterpriseServices;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,7 @@ namespace MvcOnlineTicariOtomasyon
     {
         protected void Application_Start()
         {
+            GlobalFilters.Filters.Add(new AuthorizeAttribute());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);

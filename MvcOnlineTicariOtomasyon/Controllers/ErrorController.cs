ea8fda9af27f26c2946d66487b616f832a9a298f
İsmail Baran <ismailbaran04@gmail.com>
2023-12-28 02:04:47 +0300

    error sayfası ve controllerı eklendi

---
+++ MvcOnlineTicariOtomasyon/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult PageError()
+        {
+            Response.TrySkipIisCustomErrors = true;
+            return View();
+        }
+        public ActionResult Page400() {
+            Response.StatusCode = 400;
+            Response.TrySkipIisCustomErrors = true;
+            return View("PageError");
+        }
+        public ActionResult Page403()
+        {
+            Response.StatusCode = 403;
+            Response.TrySkipIisCustomErrors = true;
+            return View("PageError");
+        }
+        public ActionResult Page404()
+        {
+            Response.StatusCode = 404;
+            Response.TrySkipIisCustomErrors = true;
+            return View("PageError");
+        }
+    }
+}
